0f349e2c22e825f8c8629387f4e2b106d4c7b697
Ceekha <shikha@Ceekhas-MacBook-Pro.local>
2020-06-20 18:16:26 -0400

    Using Event Handler

--- src/App.js
+++ src/App.js
@@ -12,32 +12,47 @@ class App extends Component {
     otherState: 'some other value'
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = ( newName ) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
     });
   };
 
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {/* Method 1 */}
+        <button onClick={this.switchNameHandler.bind(this,'MAXXXXXIII')}>Switch Name</button>
+        {/* Method 2 */}
+        {/* Implicitly adds return in front of anonymous function this.switchNameHandler(this,'MAXXXXXIII') */}
+         {/* <button onClick={() => this.switchNameHandler('MAXXXXXIII')}>Switch Name</button> */}
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}
+          click={this.switchNameHandler.bind(this,'max!!!!!')}
         >
           My Hobbies: Racing
         </Person>
