db74b0cc907a6e92369cd15ace03fe4910bf6ccb
Ceekha <shikha@Ceekhas-MacBook-Pro.local>
2020-06-27 14:25:41 -0400

    Adding conditional statement

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPerson: false
   };
 
   switchNameHandler = ( newName ) => {
@@ -34,16 +35,34 @@ class App extends Component {
     })
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({showPerson: !doesShow});
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font:'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         {/* Method 1 */}
-        <button onClick={this.switchNameHandler.bind(this,'MAXXXXXIII')}>Switch Name</button>
+        <button 
+        style ={style}
+        onClick={this.togglePersonsHandler}>Switch Name</button>
         {/* Method 2 */}
         {/* Implicitly adds return in front of anonymous function this.switchNameHandler(this,'MAXXXXXIII') */}
          {/* <button onClick={() => this.switchNameHandler('MAXXXXXIII')}>Switch Name</button> */}
+        
+        {
+        this.state.showPerson ? 
+          <div>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -60,7 +79,10 @@ class App extends Component {
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}
         />
+        </div> : null
+        }
       </div>
+
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
