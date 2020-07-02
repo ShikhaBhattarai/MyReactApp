59ff458f761f3feb0ad789dc13bbaf24dc69fda8
Ceekha <shikha@ceekhas-mbp.lan>
2020-07-02 12:23:44 -0400

    Rendering dynamic content

--- src/App.js
+++ src/App.js
@@ -13,20 +13,15 @@ class App extends Component {
     showPerson: false
   };
 
-  switchNameHandler = ( newName ) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
-  };
+  deletePersonHandler = (personIndex) => {
+    //Immutable
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
 
   nameChangedHandler = (event) => {
-    this.setState( {
+    this.setState({
       persons: [
         { name: 'Max', age: 28 },
         { name: event.target.value, age: 29 },
@@ -37,54 +32,63 @@ class App extends Component {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPerson;
-    this.setState({showPerson: !doesShow});
+    this.setState({ showPerson: !doesShow });
   }
 
   render() {
     const style = {
       backgroundColor: 'white',
-      font:'inherit',
+      font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
+    };
+
+    //Conditional second way
+    let persons = null;
+
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index)=> {
+            //returns new array
+            return <Person 
+              name={person.name} 
+              age={person.age}
+              click ={() => {this.deletePersonHandler(index)}} />
+          })}
+          {/* <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            changed={this.nameChangedHandler}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'max!!!!!')}
+          >
+            My Hobbies: Racing
+        </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          /> */}
+        </div>
+      )
     }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         {/* Method 1 */}
-        <button 
-        style ={style}
-        onClick={this.togglePersonsHandler}>Switch Name</button>
-        {/* Method 2 */}
-        {/* Implicitly adds return in front of anonymous function this.switchNameHandler(this,'MAXXXXXIII') */}
-         {/* <button onClick={() => this.switchNameHandler('MAXXXXXIII')}>Switch Name</button> */}
-        
-        {
-        this.state.showPerson ? 
-          <div>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          changed={this.nameChangedHandler}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this,'max!!!!!')}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-        </div> : null
-        }
-      </div>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Switch Name</button>
 
+        {persons}
+      </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
